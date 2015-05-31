030b8d82c025c882fd6423a2846e2c6c579eb885
Zhenfei You <hello@imyzf.com>
2015-05-31 20:13:16 +0800

    实现对首页进行多页抓取

--- common/models/article.js
+++ common/models/article.js
@@ -3,8 +3,27 @@ var cheerio = require('cheerio');
 
 module.exports = function (Article) {
 
-  function crawlPage(url, cb) {
-    http.get(url, function (res) {
+  function crawlPage(currentPage, maxPage, cb) {
+    if (currentPage > maxPage) {
+      cb(null, 'finish');
+      return;
+    }
+    for (var i = 0; i < 3; i++) console.log();
+    console.log('------------ page ' + currentPage + ' -------------------');
+    for (var i = 0; i < 3; i++) console.log();
+    var options = {
+      hostname: 'www.cnblogs.com',
+      path: '/mvc/AggSite/PostList.aspx',
+      method: 'POST',
+      headers: {
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Referer': 'http://www.cnblogs.com/',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4,en-US;q=0.2'
+      }
+    };
+    var req = http.request(options, function (res) {
       var html = '';
       res.on('data', function (chunk) {
         html += chunk;
@@ -26,15 +45,23 @@ module.exports = function (Article) {
           console.log('summary  ', summary);
           console.log('author  ', author);
           console.log('date  ', date);
-
+          console.log('----------------------------------------------------------');
         });
-        cb(null, 'finish');
+        crawlPage(currentPage + 1, maxPage, cb);
       })
     });
+    req.write(JSON.stringify({
+      CategoryId: 808,
+      CategoryType: "SiteHome",
+      ItemListActionName: "PostList",
+      PageIndex: currentPage,
+      ParentCategoryId: 0
+    }));
+    req.end();
   }
 
   Article.crawl = function (cb) {
-    crawlPage('http://www.cnblogs.com/', cb)
+    crawlPage(1, 3, cb)
   };
 
   Article.remoteMethod(
